b948f73907cfbb58f289125ef4429ccfc4136d70
ftcaarya <rautaarya23@gmail.com>
2024-12-24 16:46:55 -0500

    servo constants are perfected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extraneous/HardwareTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extraneous/HardwareTesting.java
@@ -4,10 +4,13 @@ import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.ARM_SER
 import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.ARM_SERVO_UP;
 import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.CLAW_CLOSE;
 import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.CLAW_OPEN;
+import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.WRIST_SERVO_DOWN;
 import static org.firstinspires.ftc.teamcode.extraneous.ServoProgramming.WRIST_SERVO_UP;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "Hardware testing", group = "exercises")
 public class HardwareTesting extends OpMode {
     ServoProgramming servo;
     AllMech robot;
@@ -36,6 +39,12 @@ public class HardwareTesting extends OpMode {
             servo.claw.setPosition(CLAW_CLOSE);
         }
 
+        if (gamepad1.dpad_down) {
+            servo.wrist.setPosition(WRIST_SERVO_DOWN);
+        }
+        if (gamepad1.dpad_up){
+            servo.wrist.setPosition(WRIST_SERVO_UP);
+        }
 
     }
 }

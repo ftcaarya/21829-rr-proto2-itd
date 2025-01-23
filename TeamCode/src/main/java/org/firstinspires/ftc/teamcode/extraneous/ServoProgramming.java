8a937a3847fce2159eb707b89a6caf1085e5218a
ftcaarya <rautaarya23@gmail.com>
2025-01-23 18:47:11 -0500

    cool teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extraneous/ServoProgramming.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extraneous/ServoProgramming.java
@@ -18,7 +18,7 @@ public class ServoProgramming {
     public static final double WRIST_SERVO_SPEC = 0.49;
 
     public static final double LEFT_ARM_SERVO_UP = .5;//.9
-    public static final double LEFT_ARM_SERVO_DOWN = .46;//.8
+    public static final double LEFT_ARM_SERVO_DOWN = .48;//.8
     public static final double LEFT_ARM_SERVO_SPEC = .46;//.83
 
     public static final double LEFT_ARM_SERVO_SCORE = .45;//.62
